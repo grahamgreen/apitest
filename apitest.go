f92ff2d328a8e612f6f2ea2e917732ac45052fe2
Graham Green <ggreen@clearcareonline.com>
2015-06-15 23:12:03 -0500

    nightly

--- apitest.go
+++ apitest.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/nu7hatch/gouuid"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/nu7hatch/gouuid"
+)
 
 type donation struct {
 	uuid   uuid.UUID
@@ -20,4 +28,15 @@ type action struct {
 	id     uuid.UUID
 	name   string
 	thingy string
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
