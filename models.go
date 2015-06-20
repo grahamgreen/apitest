dc63dc5adc2062f65e1a29303ef2d20ebaba9b9e
Graham Green <ggreen@clearcareonline.com>
2015-06-20 10:46:07 -0500

    moving from UUID to strings for make things easy

--- models.go
+++ models.go
@@ -2,27 +2,28 @@ package main
 
 //Copyright 2015 Graham Green
 
-import "github.com/nu7hatch/gouuid"
+//I'm taking out the UUID for now
+//import "github.com/nu7hatch/gouuid"
 
 //Donation an instance of an action on an acctoun
 type Donation struct {
-	ID   uuid.UUID `json:"id"`
-	From uuid.UUID `json:"from"`
-	To   uuid.UUID `json:"to"`
+	ID   string `json:"id"`
+	From string `json:"from"`
+	To   string `json:"to"`
 }
 
 //Account an account; can send donations by default
 //must have an accociated action to receive actions
 type Account struct {
-	ID     uuid.UUID `json:"id"`
-	Action uuid.UUID `json:"action"`
-	Name   string    `json:"name"`
-	Stuff  string    `json:"stuff"`
+	ID     string `json:"id"`
+	Action string `json:"action"`
+	Name   string `json:"name"`
+	Stuff  string `json:"stuff"`
 }
 
 //Action a action associated w/ an account
 type Action struct {
-	ID     uuid.UUID `json:"id"`
-	Name   string    `json:"name"`
-	Thingy string    `json:"thingy"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Thingy string `json:"thingy"`
 }
